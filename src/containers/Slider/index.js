3d77fb4bdeb4bb63eb0318e948f2ffc85c0f1bf6
DyBryck <150355330+DyBryck@users.noreply.github.com>
2024-08-20 11:21:38 +0200

    Update index.js

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -45,7 +45,7 @@ const Slider = () => {
         <div className="SlideCard__pagination">
           {byDateDesc?.map((_, radioIdx) => (
             <input
-              key={radioIdx}
+              key={_.title}
               type="radio"
               name="radio-button"
               checked={index === radioIdx}
